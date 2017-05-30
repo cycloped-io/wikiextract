cbda7f3c3ee7d1929fdcb51b94acb25b4bc2293a
Aleksander Smywinski-Pohl <apohllo@o2.pl>
2017-05-30 11:50:29 +0200

    Use melisa + simplify article reading

--- utils/count_links.rb
+++ utils/count_links.rb
@@ -4,15 +4,16 @@ require 'bundler/setup'
 require 'csv'
 require 'progress'
 require 'slop'
-require 'cyclopedio/wiki'
+require 'melisa'
 
 options = Slop.new do
-  banner "#{$PROGRAM_NAME} -t tokens_text.csv -d database -o candidates.csv\n" +
-    "Count link occurrences in Wikipedia text."
+  banner "#{$PROGRAM_NAME} -i occurrences.index -o link_counts.csv -d candidates.tsv -t tokens.tsv\n" +
+    "Count link occurrences and discover link candidates in Wikipedia text."
 
-  on :d=, :database, "Rod database with Wikipedia data", required: true
   on :t=, :tokens, "File with text tokens", required: true
+  on :i=, :index, "Input file with Marisa index", required: true
   on :o=, :counts, "Output file with link counts", required: true
+  on :c=, :candidates, "Output file with link candidates", required: true
   on :l=, :limit, "Limit for articles (last-1 article to process)", as: Integer
   on :x=, :offset, "Offset of articles (first article to process)", as: Integer, default: 0
   on :q, :quiet, "Don't print progress"
@@ -26,41 +27,47 @@ rescue => ex
   exit
 end
 
-include Cyclopedio::Wiki
-Database.instance.open_database(options[:database])
+def convert_tuple(tuple)
+  document_id, token_index, space, token = *tuple
+  token = token.chomp
+  document_id = document_id.to_i
+  [document_id, token_index, space, token]
+end
+
+def count_and_match_tokens(index, stack, input, output, counts)
+  string = stack[0][3] + stack[1..-1].map{|_, _, s, t| s == "1" ? " " + t : t}.join("")
+  string.force_encoding("ascii-8bit")
+  token_id = index[string]
+  if token_id
+    first_tuple = stack[0]
+    last_tuple = stack.last
+    output.puts "%s\t%s\t%s\t%s" % [first_tuple[0], first_tuple[1], last_tuple[1], string]
+    counts[string] += 1 if token_id
+  end
+  index.search(string).size > 0
+end
 
 counts = Hash.new(0)
-limit = options[:limit] || Article.count+1
-CSV.open(options[:tokens]) do |input|
-  Progress.start(limit-options[:offset]) unless options[:quiet]
-  last_id = nil
-  input.each do |tokens|
-    begin
-      tokens.map!(&:to_i)
-      article_id = tokens.shift
-      next if article_id < options[:offset]
-      break if article_id >= limit
-      Progress.step if !options[:quiet] && last_id != article_id
-      last_id = article_id
-      tokens.each.with_index do |token_id,index|
-	token = Token.find_by_rod_id(token_id)
-	trie = token.trie
-	current_index = index + 1
-	next if current_index >= tokens.size
-	while(trie) do
-	  counts[trie.trace] += 1
-	  trie = trie.fast_search(tokens[current_index])
-	  current_index += 1
-	  break if current_index >= tokens.size
-	end
+index = Melisa::IntTrie.new
+index.load(options[:index])
+File.open(options[:candidates], "w") do |output|
+  CSV.open(options[:tokens], col_sep: "\t", quote_char: "\x00") do |input|
+    input.each do |tuple|
+      begin
+        stack = [convert_tuple(tuple)]
+        recoreded_pos = input.pos
+        while(count_and_match_tokens(index, stack, input, output, counts)) do
+          stack << convert_tuple(input.shift)
+        end
+        input.pos = recoreded_pos
+      rescue Interrupt
+        break
       end
-    rescue Interrupt
-      break
     end
+    Progress.stop
   end
-  Progress.stop
 end
-Database.instance.close_database
+
 CSV.open(options[:counts],"w") do |output|
   Progress.start(counts.size) unless options[:quiet]
   counts.each do |tuple|
